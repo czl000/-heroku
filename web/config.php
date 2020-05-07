6622639662127fd70103174e1f45c183f91661f7
czl000 <59338364+czl000@users.noreply.github.com>
2020-05-07 21:01:51 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('APIKEY', 'AIzaSyBBOun3ZivcJitzokHofUWioYgsX4e45hQ');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'y2');//网站名字
+define('TITLENAME', 'y2');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', '1078998347@qq.com');//邮箱
 ?>
